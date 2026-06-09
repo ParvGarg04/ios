d2755cab2a21324456b587043555543dfa64e966
Parv Garg <parvgarg.sn@gmail.com>
2026-06-09 16:46:47 +0530

    Update TasksViewModel.swift

--- DailyFlow/DailyFlow/ViewModels/TasksViewModel.swift
+++ DailyFlow/DailyFlow/ViewModels/TasksViewModel.swift
@@ -1,6 +1,7 @@
 import Foundation
 import UIKit
-
+import SwiftUI    // ← add this line
+import Firebase
 @MainActor
 final class TasksViewModel: ObservableObject {
     @Published var pendingTasks:   [TaskItem] = []
